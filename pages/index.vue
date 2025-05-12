22dda1d71ac9f471677b83780ac88b6f2db1969f
martijn <martijn.de.vries@windesheim.nl>
2025-05-12 19:32:57 +0200

    removed redundant links on dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,8 @@
 
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-    <!-- Brand Name -->
     <h1 class="text-5xl font-bold text-blue-600 mb-2">VintaGo</h1>
 
-    <!-- Slogan -->
     <p class="text-xl text-gray-600 italic">Take a seat</p>
     <div class="space-y-4 mt-4">
       <NuxtLink
@@ -15,34 +13,6 @@
       >
         Ga naar de Planning
       </NuxtLink>
-      <NuxtLink
-          to="/artikel"
-          class="px-6 py-2 bg-green-500 text-white font-bold rounded-lg shadow-md hover:bg-green-700 transition duration-200"
-      >
-        Ga naar de Artikelpagina
-      </NuxtLink>
-      <NuxtLink
-          to="/dbscan"
-          class="px-6 py-2 bg-green-500 text-white font-bold rounded-lg shadow-md hover:bg-green-700 transition duration-200"
-      >
-        DBscan test
-      </NuxtLink>
-      <NuxtLink
-          to="/sa"
-          class="px-6 py-2 bg-green-500 text-white font-bold rounded-lg shadow-md hover:bg-green-700 transition duration-200"
-      >
-        Simmulated Annealing
-      </NuxtLink>
-      <NuxtLink
-          to="/simulation"
-          class="px-6 py-2 bg-green-500 text-white font-bold rounded-lg shadow-md hover:bg-green-700 transition duration-200"
-      >
-        Init simulation
-      </NuxtLink>
     </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
